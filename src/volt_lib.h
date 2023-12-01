6f973f604a1fa5d678e7ae2b131ff3b02bcb2979
vehehgito <aryann2708@gmail.com>
2023-12-01 17:54:24 +0530

    Added I/O ops >> volt_lib.h

--- src/volt_lib.h
+++ src/volt_lib.h
@@ -151,4 +151,138 @@ char* bump_alloc(BumpAllocator* bumpAllocator, size_t size)
 // File I/O
 // ###########################
 
+long long get_timestamp(const char* file)
+{
+  struct stat file_stat = {};
+  stat(file, &file_stat);
+  return file_stat.st_mtime;
+}
 
+bool file_exists(const char* filePath)
+{
+  SM_ASSERT(filePath, "No filePath supplied!");
+
+  auto file = fopen(filePath, "rb");
+  if(!file)
+  {
+    return false;
+  }
+  fclose(file);
+
+  return true;
+}
+
+long get_file_size(const char* filePath)
+{
+  SM_ASSERT(filePath, "No filePath supplied!");
+
+  long fileSize = 0;
+  auto file = fopen(filePath, "rb");
+  if(!file)
+  {
+    SM_ERROR("Failed opening File: %s", filePath);
+    return 0;
+  }
+
+  fseek(file, 0, SEEK_END);
+  fileSize = ftell(file);
+  fseek(file, 0, SEEK_SET);
+  fclose(file);
+
+  return fileSize;
+}
+
+char* read_file(const char* filePath, int* fileSize, char* buffer)
+{
+  SM_ASSERT(filePath, "No filePath supplied!");
+  SM_ASSERT(fileSize, "No fileSize supplied!");
+  SM_ASSERT(buffer, "No buffer supplied!");
+
+  *fileSize = 0;
+  auto file = fopen(filePath, "rb");
+  if(!file)
+  {
+    SM_ERROR("Failed opening File: %s", filePath);
+    return nullptr;
+  }
+
+  fseek(file, 0, SEEK_END);
+  *fileSize = ftell(file);
+  fseek(file, 0, SEEK_SET);
+
+  memset(buffer, 0, *fileSize + 1);
+  fread(buffer, sizeof(char), *fileSize, file);
+
+  fclose(file);
+
+  return buffer;
+}
+
+char* read_file(const char* filePath, int* fileSize, BumpAllocator* bumpAllocator)
+{
+  char* file = nullptr;
+  long fileSize2 = get_file_size(filePath);
+
+  if(fileSize2)
+  {
+    char* buffer = bump_alloc(bumpAllocator, fileSize2 + 1);
+
+    file = read_file(filePath, fileSize, buffer);
+  }
+
+  return file; 
+}
+
+void write_file(const char* filePath, char* buffer, int size)
+{
+  SM_ASSERT(filePath, "No filePath supplied!");
+  SM_ASSERT(buffer, "No buffer supplied!");
+  auto file = fopen(filePath, "wb");
+  if(!file)
+  {
+    SM_ERROR("Failed opening File: %s", filePath);
+    return;
+  }
+
+  fwrite(buffer, sizeof(char), size, file);
+  fclose(file);
+}
+
+bool copy_file(const char* fileName, const char* outputName, char* buffer)
+{
+  int fileSize = 0;
+  char* data = read_file(fileName, &fileSize, buffer);
+
+  auto outputFile = fopen(outputName, "wb");
+  if(!outputFile)
+  {
+    SM_ERROR("Failed opening File: %s", outputName);
+    return false;
+  }
+
+  int result = fwrite(data, sizeof(char), fileSize, outputFile);
+  if(!result)
+  {
+    SM_ERROR("Failed opening File: %s", outputName);
+    return false;
+  }
+  
+  fclose(outputFile);
+
+  return true;
+}
+
+bool copy_file(const char* fileName, const char* outputName, BumpAllocator* bumpAllocator)
+{
+  char* file = 0;
+  long fileSize2 = get_file_size(fileName);
+
+  if(fileSize2)
+  {
+    char* buffer = bump_alloc(bumpAllocator, fileSize2 + 1);
+
+    return copy_file(fileName, outputName, buffer);
+  }
+
+  return false;
+}
