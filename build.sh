46d914774c738f21a0cda8cbd0b541a4a8d4d0a7
vehehgito <aryann2708@gmail.com>
2023-12-02 21:06:06 +0530

    Changed buil.dsh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 libs="-luser32 -lopengl32 -lgdi32";
 
-warnings="-Wno-writable-strings -Wno-format-security -Wno-deprecated-declarations"
+warnings="-Wno-writable-strings -Wno-format-security -Wno-deprecated-declarations -Wno-switch"
 
 includes="-Ithird_party -Ithird_party/Include"
 
